0531b4f35852290cce535cb7ec0cf57cda607d61
Martin Becker <martin.becker@hs-duesseldorf.de>
2022-08-08 17:18:29 +0200

    Add pytorch_models._init incl. tests

---
+++ src/renda/pytorch_models/_init.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+import math
+from typing import Callable, Union
+
+import torch
+
+# List callables that may be condused with init functions
+_NO_INIT_FN = [torch.nn.init.calculate_gain]
+
+
+def _is_init_fn(fn: Callable) -> bool:
+    if not isinstance(fn, Callable):
+        raise TypeError(f"Expected callable. Got {fn}.")
+
+    return (
+        hasattr(fn, "__module__")
+        and fn.__module__ == "torch.nn.init"
+        and fn not in _NO_INIT_FN
+    )
+
+
+def _is_init_fn_name(name: str) -> bool:
+    if not isinstance(name, str):
+        raise TypeError(f"Expected string. Got {name}.")
+
+    fn = getattr(torch.nn.init, name, None)
+    return fn is not None and fn not in _NO_INIT_FN
+
+
+def _get_init_fn(name: str) -> Callable:
+    if not isinstance(name, str):
+        raise TypeError(f"Expected string. Got {name}.")
+    if not _is_init_fn_name(name):
+        raise ValueError(f"Cannot find '{name}' in 'torch.nn.init'.")
+
+    return getattr(torch.nn.init, name)
+
+
+def _get_init_fn_name(fn: Callable) -> str:
+    if not isinstance(fn, Callable):
+        raise TypeError(f"Expected callable. Got {fn}.")
+    if not _is_init_fn(fn):
+        raise ValueError(f"Cannot find '{fn}' in 'torch.nn.init'.")
+
+    return fn.__name__
+
+
+def _default_weight_init_fn(weight: torch.Tensor) -> None:
+    # Copied this from PyTorch 1.8.2 (2022-04-21) (comment included)
+    # Setting a=sqrt(5) in kaiming_uniform is the same as initializing with
+    # uniform(-1/sqrt(in_features), 1/sqrt(in_features)). For details, see
+    # https://github.com/pytorch/pytorch/issues/57109
+    torch.nn.init.kaiming_uniform_(weight, a=math.sqrt(5))
+
+
+def _default_bias_init_fn(bias: torch.Tensor, weight: torch.Tensor) -> None:
+    # Copied this from PyTorch 1.8.2 (2022-04-21)
+    fan_in, _ = torch.nn.init._calculate_fan_in_and_fan_out(weight)
+    # Modified 2022-04-25:
+    #   - prevent division by zero
+    #   - the following must hold: -bound <= bound
+    #   - note: if -bound == bound, bias is inited with zeros
+    if fan_in > 0:
+        bound = 1 / math.sqrt(fan_in)
+    else:
+        bound = 0  # pragma: no cover
+    torch.nn.init.uniform_(bias, -bound, bound)
+
+
+def _process_init_fn(fn: Union[None, str, Callable]) -> Callable:
+    if fn is None:
+        return None
+    elif isinstance(fn, str):
+        if _is_init_fn_name(fn):
+            return _get_init_fn(fn)
+        else:
+            raise ValueError(f"Cannot find '{fn}' in 'torch.nn.init'.")
+    elif isinstance(fn, Callable):
+        return fn  # Assume user defined init fn
+    else:
+        raise TypeError(f"Expected string, callable or None. Got {fn}.")
+
+
+def _process_init_fn_kwargs(kwargs: dict) -> dict:
+    if not isinstance(kwargs, dict):
+        raise TypeError(f"Expected dict. Got {kwargs}.")
+
+    return kwargs
---
+++ tests/pytorch_models/test__init.py
@@ -0,0 +1,160 @@
+# -*- coding: utf-8 -*-
+import random
+
+import pytest
+import torch
+from torch.nn.init import calculate_gain
+
+from renda.pytorch_models._init import (
+    _default_bias_init_fn,
+    _default_weight_init_fn,
+    _get_init_fn,
+    _get_init_fn_name,
+    _is_init_fn,
+    _is_init_fn_name,
+    _process_init_fn,
+    _process_init_fn_kwargs,
+)
+from renda.utils.seeding import max_seed, min_seed
+
+# Valid names according to PyTorch 1.8.1 documentation
+# Future PyTorch versions may introduce further init functions
+_INIT_FUNCTIONS = {
+    "uniform_": torch.nn.init.uniform_,
+    "normal_": torch.nn.init.normal_,
+    "constant_": torch.nn.init.constant_,
+    "ones_": torch.nn.init.ones_,
+    "zeros_": torch.nn.init.zeros_,
+    "eye_": torch.nn.init.eye_,
+    "dirac_": torch.nn.init.dirac_,
+    "xavier_uniform_": torch.nn.init.xavier_uniform_,
+    "xavier_normal_": torch.nn.init.xavier_normal_,
+    "kaiming_uniform_": torch.nn.init.kaiming_uniform_,
+    "kaiming_normal_": torch.nn.init.kaiming_normal_,
+    "orthogonal_": torch.nn.init.orthogonal_,
+    "sparse_": torch.nn.init.sparse_,
+}
+
+
+def test__is_init_fn():
+    for fn in _INIT_FUNCTIONS.values():
+        assert _is_init_fn(fn)
+
+    def local_fn():
+        pass
+
+    assert not _is_init_fn(local_fn)
+    assert not _is_init_fn(lambda: 0)
+    assert not _is_init_fn(min)  # Python built-in
+    assert not _is_init_fn(torch.squeeze)  # From other module
+    assert not _is_init_fn(calculate_gain)  # Although in init module
+
+    del local_fn
+
+    with pytest.raises(TypeError):
+        _is_init_fn("uniform_")
+    with pytest.raises(TypeError):
+        _is_init_fn(0)
+
+
+def test__is_init_fn_name():
+    for name in _INIT_FUNCTIONS.keys():
+        assert _is_init_fn_name(name)
+
+    assert not _is_init_fn_name("_")  # Unlikely init function name
+    assert not _is_init_fn_name("calculate_gain")  # Although in init module
+
+    with pytest.raises(TypeError):
+        _is_init_fn_name(torch.nn.init.uniform_)
+    with pytest.raises(TypeError):
+        _is_init_fn_name(0)
+
+
+def test__get_init_fn():
+    for fn_name, fn in _INIT_FUNCTIONS.items():
+        assert _get_init_fn(fn_name) is fn
+
+    with pytest.raises(TypeError):
+        _get_init_fn(torch.nn.init.uniform_)
+    with pytest.raises(TypeError):
+        _get_init_fn(0)
+
+    with pytest.raises(ValueError):
+        _get_init_fn("_")  # Unlikely init function name
+    with pytest.raises(ValueError):
+        _get_init_fn("calculate_gain")  # Although in init module
+
+
+def test__get_init_fn_name():
+    for fn_name, fn in _INIT_FUNCTIONS.items():
+        assert _get_init_fn_name(fn) == fn_name
+
+    with pytest.raises(TypeError):
+        _get_init_fn_name("uniform_")
+    with pytest.raises(TypeError):
+        _get_init_fn_name(0)
+
+    with pytest.raises(ValueError):
+        _get_init_fn_name(lambda: 0)
+    with pytest.raises(ValueError):
+        _get_init_fn_name(min)  # Python built-in
+    with pytest.raises(ValueError):
+        _get_init_fn_name(torch.squeeze)  # From other module
+    with pytest.raises(ValueError):
+        _get_init_fn_name(calculate_gain)  # Although in init module
+    with pytest.raises(ValueError):
+
+        def local_fn():
+            pass
+
+        _get_init_fn_name(local_fn)
+
+        del local_fn
+
+
+def test__default_init_fns():
+    seed = random.randint(min_seed(), max_seed())
+
+    torch.manual_seed(seed)
+    linear = torch.nn.Linear(10, 10, bias=True)
+
+    weight = torch.Tensor(10, 10)
+    bias = torch.Tensor(10)
+
+    torch.manual_seed(seed)
+    _default_weight_init_fn(weight)
+    _default_bias_init_fn(bias, weight)
+
+    assert (linear.weight == weight).all()
+    assert (linear.bias == bias).all()
+
+
+def test__process_init_fn():
+    assert _process_init_fn(None) is None
+
+    for fn_name, fn in _INIT_FUNCTIONS.items():
+        assert _process_init_fn(fn_name) is fn
+        assert _process_init_fn(fn) is fn
+
+    def local_fn():
+        pass
+
+    assert _process_init_fn(local_fn) is local_fn  # Assume user-defined init function
+
+    with pytest.raises(TypeError):
+        _process_init_fn(0)
+
+    with pytest.raises(ValueError):
+        _process_init_fn("_")  # Unlikely init function name
+
+
+def test__process_init_fn_kwargs():
+    kwargs = {}
+    assert _process_init_fn_kwargs(kwargs) == kwargs
+    kwargs = {"key": 0}
+    assert _process_init_fn_kwargs(kwargs) == kwargs
+
+    with pytest.raises(TypeError):
+        _process_init_fn_kwargs([0, 1, 2])
+    with pytest.raises(TypeError):
+        _process_init_fn_kwargs(0)
